cbdaa14b60190283788346c19627a2ef3ca10ed8
Diana García <dgarcia@inmegen.edu.mx>
2022-05-02 16:52:01 -0500

    Adding comments feature in backend

--- backend/src/models/post.js
+++ backend/src/models/post.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
 
 const CommentSchema = new mongoose.Schema({
-  author: {
-    type: String,
-    required: true,
-  },
-  comment: {
-    type: String,
-    required: true,
-  },
-});
+    author: {
+      type: String,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },  
+  { timestamps: true }
+);
 
 const PostSchema = new mongoose.Schema(
   {
--- backend/src/routes/posts.routes.js
+++ backend/src/routes/posts.routes.js
@@ -1,6 +1,7 @@
 //Bring router
 const {Router} = require('express')
 const {getPosts, getPost, createPost, updatePost, deletePost} = require('../controller/posts')
+const {listComments, saveComment, updateComment, deleteComment} = require("../controller/comments-controller")
 
 //Instance router
 const router = Router()
@@ -11,6 +12,9 @@ router.get('/post/:id', getPost)
 router.put('/post/:id', updatePost)
 router.delete('/post/:id', deletePost)
 
-
+router.get("/post/:id/comments", listComments);
+router.post("/post/:id/comments", saveComment);
+router.put("/post/:id/comments/:commentId", updateComment);
+router.delete("/post/:id/comments/:commentId", deleteComment);
 
 module.exports = router
